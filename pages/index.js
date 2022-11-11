81dde84564777ec454454961f4b716ee35b2f256
ThaisCampos <thaisc.carvalho4@gmail.com>
2022-11-10 21:29:11 -0300

    atulização da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 import config from "../config.json";
-
 import styled from "styled-components";
 import { CSSReset } from "../components/CSSReset_index";
 import Menu from "../components/Menu";
 import { StyledTimeline } from "../components/Timeline";
+import Image from 'next/image'
+import BannerImg from "../images/banner.jpg"
+import ImgBanner from "../images/code.jpg"
+import { useState } from "react";
+import React from "react";
 
 function HomePage() {
   //const estilo = {backgroundColor:"white",fontSize:34 }
 
   //console.log(config.playlists)
+
+  const [valorDoFiltro,setValorDoFiltro] = React.useState("");
+
+  //const valorDoFiltro = "Frost";
   return (
     <>
       <CSSReset />
       <div>
-        <Menu/>
-        <Header />
-        <TimeLine playlists={config.playlists} />
+       
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+        
+        <Header/>
+        <Timeline searchValue={valorDoFiltro} playlists={config.playlists}/>
+       
       </div>
     </>
   );
@@ -34,7 +45,7 @@ const StyledHeader = styled.div`
     border-radius: 50%;
   }
   .user-info {
-    margin-top:50px;
+    
     display: flex;
     align-items: center;
     width: 100%;
@@ -43,10 +54,22 @@ const StyledHeader = styled.div`
   }
 `;
 
+const StyledBanner = styled.div`
+  background-color:blue;
+  height:230px;
+  //background-image:url(${config.bg});
+  background-image:url(${({bg})=> bg})
+
+
+`
+
+
 function Header() {
   return (
     <StyledHeader>
+      <StyledBanner bg={config.bg}/>  
       <section className="user-info">
+      
         <img src={`http://github.com/${config.github}.png`} />
         <div>
           <h2>{config.name}</h2>
@@ -54,10 +77,25 @@ function Header() {
         </div>
       </section>
     </StyledHeader>
+   
   );
 }
 
-function TimeLine(props) {
+
+
+/*  DESAFIO FEITO PARA COLOCAR O BANNER
+
+function Banner(){
+  return (
+    <div>
+      <Image src={ImgBanner} style={{width:"100%", height:"300px", objectFit:"cover" }}
+      />
+    </div>
+  )
+}
+*/
+
+function Timeline({ searchValue, ...props}) {
   //console.log("teste props", props);
   const playlistNames = Object.keys(props.playlists);
 
@@ -68,14 +106,20 @@ function TimeLine(props) {
     <StyledTimeline>
       {playlistNames.map((playlistName) => {
         const videos = props.playlists[playlistName];
-        console.log(videos);
+        //console.log(videos);
         return (
-          <section>
+          <section key={playlistName}>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
+              {videos.
+              filter((video)=>{
+                const titleNormalized= video.title.toLowerCase();
+                const searchValueNormalized = searchValue.toLowerCase();
+                return titleNormalized.includes(searchValueNormalized);
+              }).
+              map((video) => {
                 return (
-                  <a>
+                  <a key={video.url} href={video.url}>
                     <img src={video.thumb} />
                     <span>{video.title}</span>
                   </a>
